b8227d68ceef38e98b026e187064970a1c30e48a
remilekun-hub <atandaremilekun@gmail.com>
2023-08-21 18:10:56 +0100

    link text

--- src/components/Sidebaritem.tsx
+++ src/components/Sidebaritem.tsx
@@ -8,16 +8,13 @@ type SidebarType = {
 
 function Sidebaritem({ link, icon, title }: SidebarType) {
   const { pathname } = useLocation();
-  const isActive = pathname === link;
+  // const isActive = pathname === link;
   return (
     <Link
       to={link}
       className={`group flex items-center hover:bg-[#2B3E46] gap-4 py-3 mb-2 px-4 rounded-[4px]`}
     >
-      <img
-        src={icon}
-        className={`w-5 h-5 ${isActive ? "brightness-100" : "brightness-[.9]"}`}
-      />
+      <img src={icon} className={`w-5 h-5`} />
       <p
         className={`group-hover:text-white  leading-[15px] ${
           pathname === link ? "text-white" : "text-[#a9b1bc]"
