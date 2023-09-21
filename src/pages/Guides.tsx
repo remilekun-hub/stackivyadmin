58cfb799473c6935ee38a312e4ee044ff7215d7f
remilekun-hub <atandaremilekun@gmail.com>
2023-09-21 18:15:52 +0100

    fixed deployment logs

--- src/pages/Guides.tsx
+++ src/pages/Guides.tsx
@@ -2,78 +2,77 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import chev from "../assets/chev.png";
 import chev2 from "../assets/chev2.png";
-import { ColumnDef } from "@tanstack/react-table";
-import { BlogType } from "@/dummy/blog";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Button } from "@/components/ui/button";
-import { MoreHorizontal } from "lucide-react";
+// import { ColumnDef } from "@tanstack/react-table";
+// import { BlogType } from "@/dummy/blog";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "@/components/ui/dropdown-menu";
+// import { Button } from "@/components/ui/button";
+// import { MoreHorizontal } from "lucide-react";
 
-import { blog } from "../dummy/blog";
-import BlogTable from "@/components/BlogTable";
+// import { blog } from "../dummy/blog";
+// import BlogTable from "@/components/BlogTable";
 
 function Guides() {
-  const columns: ColumnDef<BlogType>[] = [
-    { accessorKey: "title", header: "TITLE" },
-    {
-      accessorKey: "author",
-      header: "AUTHOR",
-      cell: ({ row }) => {
-        const blog = row.original;
-        return (
-          <div className="flex items-center gap-2">
-            <img src={blog.authorimg} alt={blog.author} className="w-6 h-6" />
-            <span>{blog.author}</span>
-          </div>
-        );
-      },
-    },
-    { accessorKey: "category", header: "CATEGORY" },
-    { accessorKey: "tags", header: "TAGS" },
-    {
-      accessorKey: "date",
-      header: "DATE",
-      cell: ({ row }) => {
-        const blog = row.original;
-        return (
-          <div className="flex flex-col gap-[px]">
-            <span>{blog.date}</span>
-            <span className="text-[#9CA3AF] font-medium">{blog.time}</span>
-          </div>
-        );
-      },
-    },
-    {
-      id: "actions",
-      header: "ACTION",
-      cell: () => {
-        // const blog = row.original;
+  // const columns: ColumnDef<BlogType>[] = [
+  //   { accessorKey: "title", header: "TITLE" },
+  //   {
+  //     accessorKey: "author",
+  //     header: "AUTHOR",
+  //     cell: ({ row }) => {
+  //       const blog = row.original;
+  //       return (
+  //         <div className="flex items-center gap-2">
+  //           <span>{blog.author}</span>
+  //         </div>
+  //       );
+  //     },
+  //   },
+  //   { accessorKey: "category", header: "CATEGORY" },
+  //   { accessorKey: "tags", header: "TAGS" },
+  //   {
+  //     accessorKey: "date",
+  //     header: "DATE",
+  //     cell: () => {
+  //       // const blog = row.original;
+  //       return (
+  //         <div className="flex flex-col gap-[px]">
+  //           {/* <span>{blog.date}</span>
+  //           <span className="text-[#9CA3AF] font-medium">{blog.time}</span> */}
+  //         </div>
+  //       );
+  //     },
+  //   },
+  //   {
+  //     id: "actions",
+  //     header: "ACTION",
+  //     cell: () => {
+  //       // const blog = row.original;
 
-        return (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" className="h-8 w-8 p-0">
-                <span className="sr-only">Open menu</span>
-                <MoreHorizontal className="h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem
-                className="cursor-pointer flex items-center gap-2"
-                // onClick={() => navigate(`/startup/${blog.id}`)}
-              >
-                <span>Delete Post</span>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        );
-      },
-    },
-  ];
+  //       return (
+  //         <DropdownMenu>
+  //           <DropdownMenuTrigger asChild>
+  //             <Button variant="ghost" className="h-8 w-8 p-0">
+  //               <span className="sr-only">Open menu</span>
+  //               <MoreHorizontal className="h-4 w-4" />
+  //             </Button>
+  //           </DropdownMenuTrigger>
+  //           <DropdownMenuContent align="end">
+  //             <DropdownMenuItem
+  //               className="cursor-pointer flex items-center gap-2"
+  //               // onClick={() => navigate(`/startup/${blog.id}`)}
+  //             >
+  //               <span>Delete Post</span>
+  //             </DropdownMenuItem>
+  //           </DropdownMenuContent>
+  //         </DropdownMenu>
+  //       );
+  //     },
+  //   },
+  // ];
   return (
     <section className="">
       <Navbar>
@@ -121,7 +120,7 @@ function Guides() {
           </div>
           {/* main contents here */}
           <div className="mt-4 pb-6 px-4 lg:px-7 mr-2">
-            <BlogTable columns={columns} data={blog} />
+            {/* <BlogTable columns={columns} data={blog} /> */}
           </div>
         </div>
       </main>
